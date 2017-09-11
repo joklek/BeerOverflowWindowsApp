457ef28d9c406202e2f4c5d57b7b90a423c9173c
CactusGreen <paulius.rudzianskas@gmail.com>
2017-09-11 21:39:07 +0300

    Geolocation

--- BeerOverflowWindowsApp/Form1.cs
+++ BeerOverflowWindowsApp/Form1.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static TestProject.DataModels.GeodataDataModel;
 
 namespace BeerOverflowWindowsApp
 {
@@ -15,6 +18,40 @@ namespace BeerOverflowWindowsApp
         public Form1()
         {
             InitializeComponent();
+            latitudeBox.Text = "54.684815";
+            longitudeBox.Text = "25.288464";
+            radiusTextBox.Text = "500";
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetBarDataAsync(latitudeBox.Text, longitudeBox.Text, radiusTextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong");
+            }
+        }
+        private async Task GetBarDataAsync(string latitude, string longitude, string radius)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0},{1}&radius={2}&type=bar&key=AIzaSyBqe4VYJPO86ui1aOtmpxapqwI3ET0ZaMY", latitude, longitude, radius));
+                var result = JsonConvert.DeserializeObject<PlacesApiQueryResponse>(response);
+                DisplayData(result);
+            }
+
+        }
+
+        public void DisplayData(PlacesApiQueryResponse resultData)
+        {
+            foreach (var result in resultData.results)
+            {
+                resultTextBox.AppendText(result.name);
+                resultTextBox.AppendText(Environment.NewLine);
+            }
         }
     }
 }
