578a63fe7dd30ecfad7c9433d0cd00892f7a6582
joklek <jokubas.lekevicius@gmail.com>
2017-09-17 21:41:41 +0300

    Implement Bar class
    Create Bar class, which holds Bar. name. Also thinking that it should hold info where the bar was found as "Vendor" string.

---
+++ BeerOverflowWindowsApp/Bar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerOverflowWindowsApp
+{
+    class Bar
+    {
+        private string Name;
+        private string Vendor;
+
+        public Bar ()
+        {
+            Name = "";
+            Vendor = "";
+        }
+        public Bar (string name, string vendor)
+        {
+            Name = name;
+            Vendor = vendor;
+        }
+
+        public string GetName ()
+        {
+            return Name;
+        }
+
+        public string GetVendor ()
+        {
+            return Vendor;
+        }
+
+        public void SetName (string name)
+        {
+            Name = name;
+        }
+
+        public void setVendor (string vendor)
+        {
+            Vendor = vendor;
+        }
+    }
+}
